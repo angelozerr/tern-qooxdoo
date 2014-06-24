77030d645cccaa09aa18d376fc104614424645a6
angelozerr <angelo.zerr@gmail.com>
2014-06-24 18:18:48 +0200

    Manage methods.

--- generator/QooxdooApi2TernDef.js
+++ generator/QooxdooApi2TernDef.js
@@ -4,8 +4,8 @@
     if (typeof define == "function" && define.amd)
 	return define([ "exports" ], mod); // AMD
     mod(root.QooxdooApi2TernDef || (root.QooxdooApi2TernDef = {})); // Plain
-								    // browser
-								    // env
+    // browser
+    // env
 })
 	(
 		this,
@@ -28,111 +28,154 @@
 			    "!define" : {}
 			};
 		    }
-		    
+
 		    var visitors = {
-			    "desc" : {
-				visit : visitDesc
-			    },
-			    "methods-static" : {
-				visit : visitMethods
-			    },
-			    "constants" : {
-				visit : visitConstants
-			    },
-			    "constructor" : {
-				visit : visitConstructor,
-				prototype : true
-			    },
-			    "methods" : {
-				visit : visitMethods,
-				prototype : true
-			    },
-			    "params" : {
-				visit : visitParams
-			    },
-			    "param" : {
-				visit : visitParam
-			    },
-			    "types" : {
-				visit : visitTypes
-			    }
-			    
+			"desc" : {
+			    visit : visitDesc
+			},
+			"methods-static" : {
+			    visit : visitMethods
+			},
+			"constants" : {
+			    visit : visitConstants
+			},
+			"constructor" : {
+			    visit : visitConstructor
+			},
+			"methods" : {
+			    visit : visitMethods,
+			    prototype : true
+			},
+			"types" : {
+			    visit : visitTypes
+			}
+
 		    }
 
 		    Generator.prototype.addApi = function(qooxdooApi) {
 			if (qooxdooApi.attributes) {
-			var name = qooxdooApi.attributes.fullName, superClass = qooxdooApi.attributes.superClass;
-			if (name) {
-			    var ternClass = getTernClass(name, this.ternDef);
-			    if (superClass) ternClass["!proto"] = superClass;
-			    visitChildren(qooxdooApi, ternClass, this.options);
-			}			
+			    var name = qooxdooApi.attributes.fullName, superClass = qooxdooApi.attributes.superClass;
+			    if (name) {
+				var ternClass = getTernClass(name, this.ternDef);
+				if (superClass)
+				    ternClass["!proto"] = superClass;
+				visitChildren(qooxdooApi, ternClass,
+					this.options);
+			    }
 			}
 		    };
-		    
+
 		    function visitChildren(qooxdoItem, ternClass, options) {
 			var children = qooxdoItem.children;
-			    if (children) {
-				for (var i = 0; i < children.length; i++) {
-				    var child = children[i];
-				    var visitor = visitors[child.type];
-				    if (visitor) {
-					var ternItem = visitor.prototype ? getTernPrototype(ternClass) : ternClass;
-					visitor.visit(child, ternItem, options);
-				    }
+			if (children) {
+			    for (var i = 0; i < children.length; i++) {
+				var child = children[i];
+				var visitor = visitors[child.type];
+				if (visitor) {
+				    var ternItem = visitor.prototype ? getTernPrototype(ternClass)
+					    : ternClass;
+				    visitor.visit(child, ternItem, options);
 				}
 			    }
+			}
 		    }
-		    
+
 		    function getTernPrototype(ternClass) {
 			if (!ternClass.prototype)
 			    ternClass.prototype = {};
 			return ternClass.prototype;
 		    }
-		    
+
 		    function visitDesc(qooxdooItem, ternItem, options) {
-			if (options.doc && qooxdooItem.attributes && qooxdooItem.attributes.text) {
+			if (options.doc && qooxdooItem.attributes
+				&& qooxdooItem.attributes.text) {
 			    ternItem["!doc"] = qooxdooItem.attributes.text;
 			}
 		    }
-		    
+
 		    function visitMethods(qooxdoItem, ternItem, options) {
 			var qooxdooMethods = qooxdoItem.children;
 			for (var i = 0; i < qooxdooMethods.length; i++) {
-			    var qooxdooMethod = qooxdooMethods[i], name = qooxdooMethod.attributes.name;
+			    var qooxdooMethod = qooxdooMethods[i], name = qooxdooMethod.attributes.name, children = qooxdooMethod.children;
 			    var ternMethod = ternItem[name] = {};
-			    ternMethod["!type"] = "fn(";
-			    visitChildren(qooxdooMethod, ternMethod, options);
-			    ternMethod["!type"] += ")";
+			    updateMethod(qooxdooMethod, ternMethod, options);
 			}
-		    }		
-		    
-		    function visitConstants(qooxdooItem, ternItem, options) {
-			
 		    }
 		    
-		    function visitParams(qooxdooItem, ternItem, options) {
-			visitChildren(qooxdooItem, ternItem, options);			
-		    }		    
-
-		    function visitParam(qooxdooItem, ternItem, options) {
-			var attributes = qooxdooItem.attributes;
-			if (attributes && attributes.name) {
-			    if (ternItem["!type"] != "fn(") {
-				ternItem["!type"] += ", ";
+		    function updateMethod(qooxdooMethod, ternMethod, options) {
+			var children = qooxdooMethod.children, type = "fn(", params, returnValue;
+			    if (children) {
+				for (var j = 0; j < children.length; j++) {
+				    var child = children[j];
+				    if (child.type === 'params') {
+					params = getParams(child);
+				    } else if (child.type === 'return') {
+					returnValue = getType(child);
+				    } else if (child.type === 'desc') {
+					visitDesc(child, ternMethod, options);
+				    }
+				}
 			    }
-			    ternItem["!type"] += attributes.name;
-			    visitChildren(qooxdooItem, ternItem, options);
+			    if (params)
+				type += params;
+			    type += ")";
+			    if (returnValue) {
+				type += " -> ";
+				type += getTernType(returnValue);
+			    }
+			    ternMethod["!type"] = type;
+		    }
+
+		    function getType(child) {
+			var children = child.children;
+			if (children) {
+			    for (var i = 0; i < children.length; i++) {
+				var c = children[i];
+				if (c.type === 'types') {
+				    var types = c.children;
+				    for (var j = 0; j < types.length; j++) {
+					return types[j].attributes.type;
+				    }
+				}
+			    }
 			}
 		    }
-		    
-		    function visitTypes(qooxdooItem, ternItem, options) {
-			if (qooxdooItem.children && qooxdooItem.children.length > 0) {
-			    var type = getTernType(qooxdooItem.children[0].attributes.type);
-			    //ternItem["!type"] += (": " + type);
-			}		
+
+		    function getParams(child) {
+			var children = child.children;
+			if (children) {
+			    var params = '';
+			    for (var i = 0; i < children.length; i++) {
+				var c = children[i];
+				if (c.type === 'param') {
+				    var name = c.attributes.name, optional = c.attributes.optional, type = getTernType(getType(c));
+
+				    if (params != '') {
+					params += ', ';
+				    }
+				    params += name;
+				    if (optional) params += '?';
+				    params += ': ';
+				    params += type;
+
+				}
+			    }
+			    return params;
+			}
 		    }
-		    
+
+		    function visitConstants(qooxdooItem, ternItem, options) {
+
+		    }
+
+		    function visitTypes(qooxdooItem, ternItem, options) {
+			if (qooxdooItem.children
+				&& qooxdooItem.children.length > 0) {
+			    var type = getTernType(qooxdooItem.children[0].attributes.type);
+			    ternItem["!type"] += (": " + type);
+			}
+		    }
+
 		    function getTernType(qooxdooType) {
 			switch (qooxdooType) {
 			case 'String':
@@ -142,136 +185,17 @@
 			case 'Number':
 			    return 'number';
 			case 'Function':
-			    return 'fn()';			    
+			    return 'fn()';
+			case 'var':
+			case 'Object':
+			    return 'Object';
 			}
 			return "+" + qooxdooType;
 		    }
-		    
+
 		    function visitConstructor(qooxdooItem, ternItem, options) {
-			
-		    }		    		    
-		    
-		    // -------------- Visit Members
-
-		    /**
-		     * Visit members.
-		     */
-		    function visitMembers(qooxdooApi, ternDef, options) {
-			var members = qooxdooApi.members;
-			if (members) {
-			    var constructorMember = getConstructorMember(members);
-			    if (constructorMember) {
-				var ternMember = getTernClass(qooxdooApi.name, ternDef);
-				var ternType = getTernType(constructorMember, true);
-				if (ternType)
-				    ternMember["!type"] = ternType;		
-			    }
-			    for (var i = 0; i < members.length; i++) {
-				var member = members[i];
-				if (!isMemberPrivate(member)
-					|| options["private"]) {
-				    var ternMember = visitMember(member,
-					    qooxdooApi, ternDef);
-				    if (ternMember)
-					addDocIfNeeded(member, ternMember,
-						options);
-				}
-			    }
-			}
+			updateMethod(qooxdooItem, ternItem, options);
 		    }
-		    
-		    function getConstructorMember(members) {
-			for (var i = 0; i < members.length; i++) {
-			    var member = members[i];
-			    if (member.name === 'constructor') {
-				return member;
-			    }
-			}
-		    }
-
-		    /**
-		     * Visit member.
-		     */
-		    function visitMember(member, qooxdooApi, ternDef) {			
-			if (member.name != 'constructor') {			    	    
-			    var ternClass = getTernClassOrPrototype(member, qooxdooApi, ternDef);
-			    var ternMember = {};
-			    ternClass[member.name] = ternMember;
-			    var ternType = getTernType(member);
-			    if (ternType)
-				ternMember["!type"] = ternType;
-			    return ternMember;
-			}
-		    }
-
-		    function isMemberChainable(member) {
-			return getMemberProperty(member, "chainable");
-		    }
-		    
-		    function isMemberPrivate(member) {
-			return getMemberProperty(member, "private");
-		    }
-
-		    function isMemberStatic(member) {
-			return getMemberProperty(member, "static");
-		    }
-
-		    function getMemberProperty(member, name) {
-			if (member[name])
-			    return member[name];
-			if (member.autodetected && member.autodetected[name])
-			    return member.autodetected[name];
-		    }
-
-		    function getOLDTernType(member, isConstructor) {
-			var memberType = member.type, memberParams = member.params, memberReturn = member.return;
-			if (!memberType) {
-			    if (memberParams || memberReturn) {
-				var fnType = 'fn(';
-				// it's a function with parameters
-				if (memberParams) {
-				for (var i = 0; i < memberParams.length; i++) {
-				    var param = memberParams[i], name = param.name, optional = param.optional, type = getTernType(
-					    param.type, null)
-				    if (i > 0)
-					fnType += ', ';
-				    fnType += name;
-				    if (optional)
-					fnType += '?';
-				    fnType += ': ';
-				    if (type) {
-					fnType += type;
-				    } else {
-					fnType += '?';
-				    }
-				}
-				}
-				fnType += ')';
-				if (!isConstructor) {
-				    if (memberReturn) {
-					var returnType = getTernType(memberReturn);
-					if (!returnType) returnType = '?';
-					fnType += ' -> ';
-					fnType += returnType;
-				    } else if (isMemberChainable(member)) {
-					fnType += ' -> !this';
-				    }
-				}				
-				return fnType;
-			    }
-			    return null;
-			}
-			switch (memberType) {
-			case 'String':
-			    return 'string';
-			case 'Boolean':
-			    return 'bool';
-			case 'Number':
-			    return 'number';
-			}
-			// console.log(memberType)
-		    }
-
 
 		    function getTernClass(owner, ternDef) {
 			var ternClass = ternDef;
@@ -284,12 +208,6 @@
 			    ternClass = ternClass[name];
 			}
 			return ternClass;
-		    }
-
-		    function addDocIfNeeded(qooxdooItem, ternItem, options) {
-			if (options.doc && qooxdooItem.attributes && qooxdooItem.attributes.text) {
-			    ternItem["!doc"] = qooxdooItem.attributes.text;
-			}
 		    }
 
 		});
